98bbd5b4bcf09baefde604a03713912139f62162
Thomas Sandmann <sandmann@dnli.com>
2018-04-14 11:40:23 -0700

    added lookup_biosamples function that leverages the rentrez package to retrieve data from the NCBI's Biosample database. Note: this adds a number of dependencies, e.g. the rentrez and xml packages. The DESCRIPTON and NAMESPACE files haven't been updated, yet.

--- R/geo-utils.R
+++ R/geo-utils.R
@@ -18,3 +18,91 @@ geo_id_type <- function(id) {
   tibble(id = id, type = type)
 }
 
+#' Retrieve sample annotations from NCBI's Biosample database
+#'
+#' This function uses the \code{rentrez} package to retrieve sample annotations
+#' from NCBI's Biosample database.
+#' @param x Character vector of sample identifiers to search the Biosample
+#' database for. Typically either \code{BioSample (SAMN)}, \code{SRA (SRS)} or
+#' \code{GEO (GSM)} accession numbers.
+#' @param retmax Scalar integer, the maximum number of (total) matches to
+#' retrieve from Entrez. See
+#' \url{https://www.ncbi.nlm.nih.gov/books/NBK25499/#_chapter4_ESearch_}
+#' for details. The number of records that can be retrieved in one query
+#' must be < 100,000.
+#' @return A tbl_df data.frame with sample annotations. Column names and numbers
+#' vary depending on the attributes available in the Biosample database.
+#' @export
+#' @importFrom magrittr "%>%" 
+#' @importFrom rentrez entrez_search entrez_fetch
+#' @importFrom xml2 read_xml xml_find_all xml_text xml_attr xml_find_first
+#' xml_children
+#' @importFrom purrr map
+#' @importFrom tibble set_tidy_names
+#' @importFrom tidyr unnest spread
+#' @importFrom dplyr rename rename_all
+#' @importFrom readr type_convert cols
+#' @source https://www.ncbi.nlm.nih.gov/books/NBK25499/#_chapter4_ESearch_
+#' @examples
+#' if (interactive()) {
+#'  # BioSample identifiers
+#'  lookup_biosamples(c("GSM1947162", "GSM1947179"))
+#'  # mixed SRS and GSM identifiers
+#'  lookup_biosamples(c("SRS1171537", "SRS1171536", "GSM1947179"))
+#'  # mixed samples from two different studies (with different attributes)
+#'  lookup_biosamples(c("SRS1171537", "SRS1271536"))
+#' }
+lookup_biosamples <- function(x, retmax = 1e5 - 1L) {
+  if (retmax >= 1e5) stop("retmax must be < 100,000")
+  
+  search_results <- rentrez::entrez_search(
+    db = "biosample", retmax = retmax,
+    term = sprintf("%s[ACCN]", paste(x, collapse = "[ACCN] OR ")))
+  rentrez::entrez_fetch(db = "biosample", id = search_results$ids,
+                        rettype = "xml", retmax = retmax,
+                        parsed = FALSE) %>%
+    xml2::read_xml() %>%
+    xml2::xml_children() %>%
+    map(.f = function(x) {
+      tibble::data_frame(
+        Title = xml2::xml_find_first(
+          x, 
+          xpath = ".//Description/Title") %>%
+          xml2::xml_text(trim = TRUE),
+        OrganismName = xml2::xml_find_first(
+          x, 
+          xpath = ".//Description/Organism/OrganismName") %>%
+          xml2::xml_text(trim = TRUE),
+        taxonomy_id = xml2::xml_find_all(
+          x, 
+          xpath = ".//Description/Organism") %>%
+          xml2::xml_attr("taxonomy_id"),
+        BioSample = xml2::xml_find_first(
+          x, 
+          xpath = './/Ids/Id[@db="BioSample"]') %>%
+          xml2::xml_text(trim = TRUE),
+        SRA = xml2::xml_find_first(
+          x, 
+          xpath = './/Ids/Id[@db="SRA"]') %>%
+          xml2::xml_text(trim = TRUE),
+        GEO = xml2::xml_find_first(
+          x, 
+          xpath = './/Ids/Id[@db="GEO"]') %>%
+          xml2::xml_text(trim = TRUE),
+        value = xml2::xml_find_all(
+          x, 
+          xpath = ".//Attribute") %>%
+          xml2::xml_text(trim = TRUE),
+        key = xml2::xml_find_all(
+          x, 
+          xpath = ".//Attribute") %>%
+          xml2::xml_attr("attribute_name")
+      )
+    }) %>%
+    dplyr::bind_rows() %>%
+    tidyr::spread(key = key, value = value) %>%
+    tibble::set_tidy_names(syntactic = TRUE, quiet = TRUE) %>%
+    dplyr::rename(cell_type = cell.type) %>%
+    dplyr::rename_all(tolower) %>%
+    readr::type_convert(col_types = readr::cols())
+}
