622ab245577034dd997b7a0b9ced1b417f025913
lianos <lianoglou@dnli.com>
2018-06-15 13:55:57 -0700

    Adds the "missing samples" test back using the Chiu et al. (GSE43366) dataset

--- tests/testthat/test-archs4-functional.R
+++ tests/testthat/test-archs4-functional.R
@@ -18,12 +18,15 @@ test_that("feature-level metadata retrieval works", {
   expect_is(mg$h5idx, "integer")
   expect_true(all(!is.na(mg$h5idx)))
   # no duplicated symbols
-  expect_equal(sum(duplicated(mg$symbol) & !is.na(mg$symbol)), 0)
+  isna.symbol <- is.na(mg$symbol) |
+    tolower(mg$symbol) == "na" |
+    tolower(mg$symbol) == "null"
+  expect_equal(sum(duplicated(mg$symbol) & !isna.symbol), 0)
 
   # There are some entries that we couldn't get identifiers for, but the ones
   # we got should all be prefixed with the mouse prefix.
-  mg.ens <- filter(mg, !is.na(gene_id))
-  expect_true(all(substr(mg.ens$gene_id, 1, 7) == "ENSMUSG"))
+  mg.ens <- filter(mg, !is.na(ensembl_id))
+  expect_true(all(substr(mg.ens$ensembl_id, 1, 7) == "ENSMUSG"))
 
 
   hg <- feature_info(a4, feature_type = "gene", source = "human",
@@ -36,6 +39,6 @@ test_that("feature-level metadata retrieval works", {
   # no duplicated symbols
   expect_equal(sum(duplicated(hg$symbol) & !is.na(hg$symbol)), 0)
 
-  hg.ens <- filter(hg, !is.na(gene_id))
-  expect_true(all(substr(hg.ens$gene_id, 1, 4) == "ENSG"))
+  hg.ens <- filter(hg, !is.na(ensembl_id))
+  expect_true(all(substr(hg.ens$ensembl_id, 1, 4) == "ENSG"))
 })
--- tests/testthat/test-sample-retrieval.R
+++ tests/testthat/test-sample-retrieval.R
@@ -8,32 +8,6 @@ if (!exists("a4")) {
 }
 
 
-test_that("(archs4_)sample_status identifies samples missing from GEO series", {
-  missing.none <- "GSE52564" # The Ben Barres dataset has all samples in ARCHS4
-  missing.some <- "GSE89189" # The blurton jones iPSC paper is missing some
-
-  ss.none <- series_status(a4, missing.none)
-  expect_true(all(ss.none[["in_archs4"]]))
-
-  ss.some <- series_status(a4, missing.some)
-  expect_true(!all(ss.some[["in_archs4"]]))
-})
-
-test_that("(archs4_)sample_info warns when querying series with missing samples", {
-  missing.none <- "GSE52564" # The Ben Barres dataset has all samples in ARCHS4
-  missing.some <- "GSE89189" # The blurton jones iPSC paper is missing some
-
-  # This series should have no missing samples
-  si.none <- expect_silent(sample_info(a4, missing.none))
-
-  # This series has a few missing samples
-  wregex <- sprintf("%s series .*missing samples", missing.some)
-  status.some <- expect_warning(sample_info(a4, missing.some), wregex)
-
-  # as.DGEList should also warn when we are missng samples
-  y <- expect_warning(as.DGEList(a4, missing.some), wregex)
-})
-
 test_that("All expected samples come back when queried by series", {
   series <- "GSE52564"
   expected <- sample_table(a4) %>%
@@ -46,62 +20,17 @@ test_that("All expected samples come back when queried by series", {
   expect_equal(nrow(full), nrow(expected))
 })
 
-test_that("sample_info returns desired covariate columns", {
-  # the code here looks a bit convoluted because it should be cleaned/updated
-  # to support testing "universal" covariates, mouse- and human-only covariates
-  # as well.
+test_that("(archs4_)sample_status identifies samples missing from GEO series", {
+  missing.none <- "GSE52564" # The Ben Barres dataset has all samples in ARCHS4
+  # The blurton jones dataset (GSE89189) used to be missing some, but those have
+  # been filled out
+  missing.some <- "GSE43366" # The Chiu et al. SOD1 datasets is missing some
 
-  ids.all <- tribble(
-    ~id,         ~type,            ~organism,        ~complete,
-    "GSE69354",  "series",         "mouse",          TRUE,
-    "GSE79525",  "series",         "mouse",          TRUE,
-    "GSE98041",  "series",         "mouse",          TRUE,
-    "GSE85702",  "series",         "mouse",          FALSE,
-    "GSE99095",  "series",         "human",          FALSE,
-    "GSE88681",  "series",         "human",          TRUE)
+  ss.none <- series_status(a4, missing.none)
+  expect_true(all(ss.none[["in_archs4"]]))
 
-  ids.query <- tribble(
-    ~id,          ~type,     ~organism,
-    "GSE69354",   "series",  "mouse",
-    "GSE88681",   "series",  "human",
-    "GSM1095128", "sample",  "mouse",
-    "GSM1095129", "sample",  "mouse",
-    "GSM1095130", "sample",  "mouse")
-
-  def.cols <- c("Sample_title", "Sample_source_name_ch1")
-  extra.cols <- c("Sample_molecule_ch1", "Sample_treatment_protocol_ch1",
-                  "Sample_description")
-
-  # The human data have these covariates that are not in mouse:
-  # * Sample_contact_laboratory
-  # * Sample_description
-  # * Sample_supplementary_file_2
-  h.only <- c("Sample_contact_laboratory", "Sample_description",
-              "Sample_supplementary_file_2")
-  #
-  # The mouse data have these covariates thata are not in human:
-  # * Sample_contact_state
-  # * Sample_growth_protocol_ch1
-  # * Sample_treatment_protocol_ch1
-  m.only <- c("Sample_contact_state", "Sample_growth_protocol_ch1",
-              "Sample_treatment_protocol_ch1")
-
-  all.cols <- c(def.cols, extra.cols)
-  info <- sample_info(a4, ids.query$id, all.cols)
-
-  for (col in all.cols) {
-    expect_is(info[[col]], "character", info = col)
-  }
-
-  info.m <- filter(info, organism == "mouse")
-  for (col in intersect(h.only, colnames(info))) {
-    expect_true(all(is.na(info.m[[col]])), info = col)
-  }
-
-  info.h <- filter(info, organism == "human")
-  for (col in intersect(m.only, colnames(info))) {
-    expect_true(all(is.na(info.h[[col]])), info = col)
-  }
+  ss.some <- series_status(a4, missing.some)
+  expect_true(!all(ss.some[["in_archs4"]]))
 })
 
 test_that("sample_info call handlies missing IDs gracefully", {
@@ -142,3 +71,85 @@ test_that("sample_info call handlies missing IDs gracefully", {
   expect_setequal(ex.missing, amiss$sample_id)
   expect_true(all(is.na(amiss$organism)))
 })
+
+# Tests that are no longer necessary in v4+ matrices (they were made for v2).
+# These tests:
+# 1. Looked for missing samples in certain seriesexercised
+# 2. Identified discordant sample covariates among the mouse and human datasets
+
+# test_that("(archs4_)sample_info warns when querying series with missing samples", {
+#   missing.none <- "GSE52564" # The Ben Barres dataset has all samples in ARCHS4
+#   missing.some <- "GSE89189" # The blurton jones iPSC paper is missing some
+#
+#   # This series should have no missing samples
+#   si.none <- expect_silent(sample_info(a4, missing.none))
+#
+#   # This series has a few missing samples
+#   wregex <- sprintf("%s series .*missing samples", missing.some)
+#   status.some <- expect_warning(sample_info(a4, missing.some), wregex)
+#
+#   # as.DGEList should also warn when we are missng samples
+#   y <- expect_warning(as.DGEList(a4, missing.some), wregex)
+# })
+
+# The v4 data matrices have the same covariates in the data matrices among
+# the mouse and human data.
+# ------------------------------------------------------------------------------
+# test_that("sample_info returns desired covariate columns", {
+#   # the code here looks a bit convoluted because it should be cleaned/updated
+#   # to support testing "universal" covariates, mouse- and human-only covariates
+#   # as well.
+#
+#   ids.all <- tribble(
+#     ~id,         ~type,            ~organism,        ~complete,
+#     "GSE69354",  "series",         "mouse",          TRUE,
+#     "GSE79525",  "series",         "mouse",          TRUE,
+#     "GSE98041",  "series",         "mouse",          TRUE,
+#     "GSE85702",  "series",         "mouse",          FALSE,
+#     "GSE99095",  "series",         "human",          FALSE,
+#     "GSE88681",  "series",         "human",          TRUE)
+#
+#   ids.query <- tribble(
+#     ~id,          ~type,     ~organism,
+#     "GSE69354",   "series",  "mouse",
+#     "GSE88681",   "series",  "human",
+#     "GSM1095128", "sample",  "mouse",
+#     "GSM1095129", "sample",  "mouse",
+#     "GSM1095130", "sample",  "mouse")
+#
+#   def.cols <- c("Sample_title", "Sample_source_name_ch1")
+#   extra.cols <- c("Sample_molecule_ch1", "Sample_treatment_protocol_ch1",
+#                   "Sample_description")
+#
+#   # The human data have these covariates that are not in mouse:
+#   # * Sample_contact_laboratory
+#   # * Sample_description
+#   # * Sample_supplementary_file_2
+#   h.only <- c("Sample_contact_laboratory", "Sample_description",
+#               "Sample_supplementary_file_2")
+#   #
+#   # The mouse data have these covariates thata are not in human:
+#   # * Sample_contact_state
+#   # * Sample_growth_protocol_ch1
+#   # * Sample_treatment_protocol_ch1
+#   m.only <- c("Sample_contact_state", "Sample_growth_protocol_ch1",
+#               "Sample_treatment_protocol_ch1")
+#
+#   all.cols <- c(def.cols, extra.cols)
+#   info <- sample_info(a4, ids.query$id, all.cols)
+#
+#   for (col in all.cols) {
+#     expect_is(info[[col]], "character", info = col)
+#   }
+#
+#   info.m <- filter(info, organism == "mouse")
+#   for (col in intersect(h.only, colnames(info))) {
+#     expect_true(all(is.na(info.m[[col]])), info = col)
+#   }
+#
+#   info.h <- filter(info, organism == "human")
+#   for (col in intersect(m.only, colnames(info))) {
+#     expect_true(all(is.na(info.h[[col]])), info = col)
+#   }
+# })
+
